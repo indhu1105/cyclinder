0ca38d0f997b1e048711ee697236c0fc1fd8b65a
indhu1105 <indhumathi.mani@trustrace.com>
2020-05-20 17:26:21 +0530

    Add js file

---
+++ cyclinder.js
@@ -0,0 +1,10 @@
+let r = +prompt("enter the radius");
+let h = +prompt("enter the height")
+let result;
+
+function area() {
+   result = (2 * 3.14 * r * h)+ (2 * 3.14 * r * r );
+   console.log(result);
+   return result;
+   }
+       
